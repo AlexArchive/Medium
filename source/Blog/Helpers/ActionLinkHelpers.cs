58dbf52aaf2d4fd362a59ad12b4bccd480ef2f4c
ByteBlast <byteblast@gmail.com>
2014-04-09 05:21:48 +0100

    added blog entry action link helper

---
+++ source/Blog/Helpers/ActionLinkHelpers.cs
@@ -0,0 +1,19 @@
+﻿using System.Web.Mvc;
+using System.Web.Mvc.Html;
+using Blog.Core.Data.Entities;
+
+namespace Blog.Helpers
+{
+    public static class ActionLinkHelpers
+    {
+        public static MvcHtmlString EntryLink(this HtmlHelper helper, BlogEntry entry)
+        {
+            return helper.ActionLink(
+                linkText: entry.Header,
+                actionName: "Entry",
+                controllerName: "Blog",
+                routeValues: new { headerSlug = entry.HeaderSlug }, 
+                htmlAttributes: null);
+        }
+    }
+}
