64be50c4cb9a54b7bfb8e40e50d41e19d190d7a9
ByteBlast <byteblast@gmail.com>
2014-04-29 11:22:17 +0100

    improved the all view.

---
+++ source/Blog/Areas/Admin/Controllers/DashboardController.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Mvc;
+
+namespace Blog.Areas.Admin.Controllers
+{
+    [Authorize]
+    public class DashboardController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
