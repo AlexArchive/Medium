0f3039b650a8fa4b172394003f31b82934ddbb8b
ByteBlast <byteblast@gmail.com>
2014-07-09 13:23:33 +0100

    Implemented slug converter.

---
+++ GoBlog.Test/SlugConverterTest.cs
@@ -0,0 +1,16 @@
+﻿using NUnit.Framework;
+
+namespace GoBlog.Test
+{
+    [TestFixture]
+    public class SlugConverterTest
+    {
+        [TestCase("Hello World", ExpectedResult = "hello-world")]
+        [TestCase("Hello, World!", ExpectedResult = "hello-world")]
+        [TestCase("   Hello   World   ", ExpectedResult = "hello-world")]
+        public string SlugConversion(string input)
+        {
+            return SlugConverter.Convert(input);
+        }
+    }
+}
---
+++ GoBlog/SlugConverter.cs
@@ -0,0 +1,17 @@
+﻿using System.Text.RegularExpressions;
+using Microsoft.Win32;
+
+namespace GoBlog
+{
+    public static class SlugConverter
+    {
+        public static string Convert(string title)
+        {
+            title = title.ToLowerInvariant();
+            title = Regex.Replace(title, "[^\\w]", "-");
+            title = Regex.Replace(title, "-+", "-");
+            title = title.Trim('-');
+            return title;
+        }
+    }
+}
