e56178cac1c6e37bb1ae3c38ec675683f0c8fcd8
ByteBlast <byteblast@gmail.com>
2014-08-21 00:21:33 +0100

    Initial commit.

---
+++ GoBlog.UnitTest/HomeControllerTest.cs
@@ -0,0 +1,17 @@
+﻿using GoBlog.Controllers;
+using NUnit.Framework;
+using TestStack.FluentMVCTesting;
+
+namespace GoBlog.UnitTest
+{
+    [TestFixture]
+    public class HomeControllerTest
+    {
+        [Test]
+        public void Index_RendersCorrectView()
+        {
+            HomeController controller = new HomeController();
+            controller.WithCallTo(c => c.Index()).ShouldRenderDefaultView();
+        }
+    }
+}
