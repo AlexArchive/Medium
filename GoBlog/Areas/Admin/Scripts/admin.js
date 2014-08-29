e2c25b5c0b82c36f896511e8654c4fbd5d908d0e
ByteBlast <byteblast@gmail.com>
2014-08-29 21:19:10 +0100

    Implemented confirm delete alert.

---
+++ GoBlog/Areas/Admin/Scripts/admin.js
@@ -0,0 +1,8 @@
+﻿$(document).ready(function () {
+    $("*[data-confirmation-msg]").click(function (event) {
+        var confirmText = $(this).attr("data-confirmation-msg");
+        if (!confirm(confirmText)) {
+            event.preventDefault();
+        }
+    });
+});
