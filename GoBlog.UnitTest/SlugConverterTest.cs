af420829614649410d1f6a0d2ffce40e8ab204d2
ByteBlast <byteblast@gmail.com>
2014-08-30 22:41:49 +0100

    Implemented title to slug converter.

---
+++ GoBlog.UnitTest/SlugConverterTest.cs
@@ -0,0 +1,16 @@
+﻿using NUnit.Framework;
+
+namespace GoBlog.UnitTest
+{
+    public class SlugConverterTest
+    {
+        [TestCase("Hello World", ExpectedResult = "hello-world")]
+        [TestCase("Hello, World!", ExpectedResult = "hello-world")]
+        [TestCase(" Hello  World ", ExpectedResult = "hello-world")]
+        [TestCase("Hello-World", ExpectedResult = "hello-world")]
+        public string Convert_ReturnsExpected(string input)
+        {
+            return SlugConverter.Convert(input);
+        }
+    }
+}
---
+++ GoBlog/SlugConverter.cs
@@ -0,0 +1,16 @@
+﻿using System.Text.RegularExpressions;
+
+namespace GoBlog
+{
+    public static class SlugConverter
+    {
+        public static string Convert(string title)
+        {
+            title = title.ToLowerInvariant();
+            title = Regex.Replace(title, "[^\\w]", "-");
+            title = Regex.Replace(title, "-+", "-");
+            title = title.Trim('-');
+            return title;
+        }
+    }
+}
