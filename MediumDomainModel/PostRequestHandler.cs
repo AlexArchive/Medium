0f88836251fad708a825e01e6949d619b4076828
ByteBlast <byteblast@gmail.com>
2014-12-05 20:01:04 +0000

    Started using Dapper for data access.

--- MediumDomainModel/PostRequestHandler.cs
+++ MediumDomainModel/PostRequestHandler.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using Dapper;
+using System.Collections.Generic;
 using System.Configuration;
 using System.Data.SqlClient;
 
@@ -8,31 +9,15 @@ namespace MediumDomainModel
     {
         public IEnumerable<PostModel> Handle()
         {
-            var connectionStr = ConfigurationManager.ConnectionStrings["Medium"].ConnectionString;
-
-            using (SqlConnection connection = new SqlConnection(connectionStr))
+            var connectionStr = ConfigurationManager
+                .ConnectionStrings["Medium"]
+                .ConnectionString;
+            using (var connection = new SqlConnection(connectionStr))
             {
-                connection.Open();
-
-                using (SqlCommand command = new SqlCommand())
-                {
-                    command.Connection = connection;
-                    command.CommandText = @"
-                        SELECT *
-                        FROM [Posts]";
-
-                    using (var reader = command.ExecuteReader())
-                    {
-                        while (reader.Read())
-                        {
-                            yield return new PostModel
-                            {
-                                Title = (string) reader["Title"],
-                                Slug = (string) reader["Slug"]
-                            };
-                        }
-                    }
-                }
+                const string commandText = @"
+                    SELECT *
+                    FROM [Posts]";
+                return connection.Query<PostModel>(commandText);
             }
         }
     }
