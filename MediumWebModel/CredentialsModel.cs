690d9bbf0c39aac1b6e4c5560789a24bb3f5865e
ByteBlast <byteblast@gmail.com>
2014-12-06 19:58:55 +0000

    Made password input show a password char.

--- MediumWebModel/CredentialsModel.cs
+++ MediumWebModel/CredentialsModel.cs
@@ -1,8 +1,12 @@
-﻿namespace Medium.WebModel
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Medium.WebModel
 {
     public class CredentialsModel
     {
         public string Username { get; set; }
+
+        [DataType(DataType.Password)]
         public string Password { get; set; }
     }
 }
