c78ec8f0b5bb7f0270642bb945272667847f928a
ByteBlast <byteblast@gmail.com>
2014-12-18 08:44:20 +0000

    Enhanced tag name formatting.
    https://github.com/AbleOpus wrote this pattern for me.

--- MediumDomainModel/TagConverter.cs
+++ MediumDomainModel/TagConverter.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text.RegularExpressions;
 
 namespace Medium.DomainModel
 {
@@ -10,11 +11,13 @@ namespace Medium.DomainModel
 
         public static IEnumerable<string> Convert(string delimitedTags)
         {
-            return delimitedTags
+            return Regex
+                .Replace(delimitedTags, @"(\s*)(,|^|$)(\s*)", "$2")
                 .Trim()
                 .Split(new[] { Delimiter }, StringSplitOptions.RemoveEmptyEntries)
                 .Distinct();
         }
+
         public static string Convert(IEnumerable<string> tags)
         {
             return string.Join(Delimiter + " ", tags);
