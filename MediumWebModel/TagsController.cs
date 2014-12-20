c26a2b454130cb6f3ce11065d589d5965460c013
ByteBlast <byteblast@gmail.com>
2014-12-20 10:26:31 +0000

    User must now confirm deletion.
    Closes #23.

---
+++ MediumWebModel/TagsController.cs
@@ -0,0 +1,21 @@
+﻿using System.Web.Mvc;
+using MediatR;
+
+namespace Medium.WebModel
+{
+    public class TagsController : Controller
+    {
+        private readonly IMediator requestBus;
+
+        public TagsController(IMediator requestBus)
+        {
+            this.requestBus = requestBus;
+        }
+
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
