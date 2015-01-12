eb994ea97af040ed9d2554ae1b4841b02e4dcefc
ByteBlast <byteblast@gmail.com>
2015-01-12 13:44:02 +0000

    Updated solution to use localdb

---
+++ MediumDomainModel/AllPostsRequest.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using MediatR;
+
+namespace Medium.DomainModel
+{
+    public class AllPostsRequest : IRequest<IEnumerable<PostModel>>
+    {
+         
+    }
+}
