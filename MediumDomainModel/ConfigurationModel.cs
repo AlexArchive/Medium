0d7ae26b9af2a0706203a4d2f3e0eaa8e071704c
ByteBlast <byteblast@gmail.com>
2015-01-29 14:22:06 +0000

    Added "about me" config

--- MediumDomainModel/ConfigurationModel.cs
+++ MediumDomainModel/ConfigurationModel.cs
@@ -1,7 +1,14 @@
-﻿namespace Medium.DomainModel
+﻿using System.ComponentModel.DataAnnotations;
+using System.Web.Mvc;
+
+namespace Medium.DomainModel
 {
     public class ConfigurationModel
     {
         public string BlogTitle { get; set; }
+
+        [DataType(DataType.MultilineText)]
+        [AllowHtml]
+        public string AboutText { get; set; } 
     }
 }
