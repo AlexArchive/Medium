b991a6ed94fcf2fd2a7200ff5a9f66748aba00bb
ByteBlast <byteblast@gmail.com>
2015-02-23 09:35:28 +0000

    Handler now deletes orphan tags

--- MediumDomainModel/DeletePostCommandHandler.cs
+++ MediumDomainModel/DeletePostCommandHandler.cs
@@ -1,4 +1,6 @@
-﻿using System.Data;
+﻿using System.Collections.Generic;
+using System.Data;
+using System.Diagnostics;
 using Dapper;
 using MediatR;
 
@@ -17,11 +19,23 @@ namespace Medium.DomainModel
         {
             var param = new { Slug = command.PostSlug };
 
+            var tags = _connection
+                .Query<string>("SELECT TagName FROM dbo.PostTagJunction WHERE PostSlug=@Slug", param);
+
             _connection
                 .Execute("DELETE FROM dbo.PostTagJunction WHERE PostSlug = @Slug", param);
 
             var successful = _connection
                 .Execute("DELETE FROM dbo.Posts WHERE Slug = @Slug", param) == 1;
+
+            foreach (var tag in tags)
+            {
+                var param2 = new { Tag = tag };
+                _connection
+                    .Execute(@"
+                        IF NOT EXISTS(SELECT 1 FROM dbo.PostTagJunction WHERE TagName = @Tag)
+                        DELETE FROM dbo.Tags WHERE Name= @Tag", param2);
+            }
 
             return successful;
         }
