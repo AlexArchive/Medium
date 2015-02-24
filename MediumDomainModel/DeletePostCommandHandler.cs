c408a370ff99689da0deea606e4266061f1b473e
ByteBlast <byteblast@gmail.com>
2015-02-24 06:35:56 +0000

    Refactored delete command handler Sql

--- MediumDomainModel/DeletePostCommandHandler.cs
+++ MediumDomainModel/DeletePostCommandHandler.cs
@@ -1,6 +1,4 @@
-﻿using System.Collections.Generic;
-using System.Data;
-using System.Diagnostics;
+﻿using System.Data;
 using Dapper;
 using MediatR;
 
@@ -8,34 +6,35 @@ namespace Medium.DomainModel
 {
     public class DeletePostCommandHandler : IRequestHandler<DeletePostCommand, bool>
     {
-        private readonly IDbConnection _connection;
+        private readonly IDbConnection connection;
 
         public DeletePostCommandHandler(IDbConnection connection)
         {
-            _connection = connection;
+            this.connection = connection;
         }
 
         public bool Handle(DeletePostCommand command)
         {
             var param = new { Slug = command.PostSlug };
 
-            var tags = _connection
-                .Query<string>("SELECT TagName FROM dbo.PostTagJunction WHERE PostSlug=@Slug", param);
+            connection.Execute(@"
+                DELETE 
+                FROM dbo.PostTagJunction 
+                WHERE PostSlug = @Slug", param);
 
-            _connection
-                .Execute("DELETE FROM dbo.PostTagJunction WHERE PostSlug = @Slug", param);
+            var successful = connection.Execute(@"
+                DELETE 
+                FROM dbo.Posts 
+                WHERE Slug = @Slug", param) == 1;
 
-            var successful = _connection
-                .Execute("DELETE FROM dbo.Posts WHERE Slug = @Slug", param) == 1;
-
-            foreach (var tag in tags)
-            {
-                var param2 = new { Tag = tag };
-                _connection
-                    .Execute(@"
-                        IF NOT EXISTS(SELECT 1 FROM dbo.PostTagJunction WHERE TagName = @Tag)
-                        DELETE FROM dbo.Tags WHERE Name= @Tag", param2);
-            }
+            connection.Execute(@"
+                DELETE 
+                FROM dbo.Tags
+                WHERE NOT EXISTS (
+                    SELECT * 
+                    FROM dbo.PostTagJunction 
+                    WHERE PostTagJunction.TagName = Tags.Name
+                )");
 
             return successful;
         }
